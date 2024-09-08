29931b3b8a4f42527b1ccb60710a63882d6f947d
Aayush Ghimire <aausgh@gmail.com>
2024-09-08 12:49:45 +0545

    Advent Calender List

--- App.tsx
+++ App.tsx
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { DayListItem } from './src/components/core/DayListItem';
 
+const days = [...Array(24)].map((val, index) => index + 1);
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <FlatList
+        data={days}
+        renderItem={({ item }) => (
+          <DayListItem day={item} />
+        )}
+        contentContainerStyle={styles.content}
+        columnWrapperStyle={styles.column}
+        numColumns={2}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +24,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+  },
+  content: {
+    padding: 10,
+    gap: 10
+  },
+  column: {
+    gap: 10
   },
 });
---
+++ src/components/core/DayListItem.tsx
@@ -0,0 +1,31 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+type DayListItemProps = {
+   day: number
+}
+
+export const DayListItem = ({ day }: DayListItemProps) => {
+   return (
+      <View style={styles.box}>
+         <Text style={styles.text}>{day}</Text>
+      </View>
+   )
+}
+
+const styles = StyleSheet.create({
+   box: {
+      backgroundColor: '#f5ebe0',
+      flex: 1,
+      aspectRatio: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 20,
+      borderWidth: StyleSheet.hairlineWidth,
+      borderColor: '#9d0208'
+   },
+   text: {
+      color: '#9d0208',
+      fontSize: 70,
+   }
+});
