5f029645bea07cca366cba0d4d4acd2f309f8699
Aayush Ghimire <aausgh@gmail.com>
2024-09-08 15:33:19 +0545

    stack navigator

---
+++ src/app/_layout.tsx
@@ -0,0 +1,12 @@
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+   return (
+      <Stack
+         screenOptions={{ headerTitleAlign: "center" }}
+      >
+         <Stack.Screen name="index" options={{ title: "DEVember" }} />
+      </Stack >
+   )
+
+}
